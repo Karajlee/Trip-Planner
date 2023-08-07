3aaede3d419adb55f7fef12fac46eabaf9836fbe
Shiqing <shiqingr@usc.edu>
2023-08-06 20:17:44 -0700

    sqlconnect
    sql connect

---
+++ connecttest.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="R6328597x")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("select * from hw4.users")
+
+result = mycursor.fetchall() # or user fetchone()
+
+for i in mycursor:
+    print(i)
